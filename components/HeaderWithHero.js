662e7dd4fb36713899c9c519e83074e5da336c9d
Greg Leeper <gregleeper@Greg-Leeper-Hugoton-USD-210.local>
2021-11-09 13:47:13 -0600

    added type to button

--- components/HeaderWithHero.js
+++ components/HeaderWithHero.js
@@ -109,7 +109,10 @@ export default function Header() {
                             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                 <div className="rounded-md shadow">
                                     <Link href="/schedules">
-                                        <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10">
+                                        <button
+                                            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10"
+                                            type="button"
+                                        >
                                             Schedules
                                         </button>
                                     </Link>
